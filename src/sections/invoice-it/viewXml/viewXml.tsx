6295237ded0369cd516a873d2bf90e1ea7b21157
Hà Đạt Cường <133970445+tommy5475000@users.noreply.github.com>
2026-03-09 15:07:24 +0700

    fix7

--- src/sections/invoice-it/viewXml/viewXml.tsx
+++ src/sections/invoice-it/viewXml/viewXml.tsx
@@ -69,7 +69,7 @@ export function ViewXml({ handleClose, rowSelect }: ViewXmlProps) {
       <DialogContent>
         {rowSelect?.file ? (
           <iframe
-            src={`http://10.1.48.35:8168/files/invoice-scan/${encodeURIComponent(fileName)}`}
+            src={`http://localhost:8168/files/invoice-scan/${encodeURIComponent(fileName)}`}
             width="100%"
             height="600px"
             style={{ border: 'none' }}
